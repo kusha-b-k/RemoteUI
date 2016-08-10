285ce74399480bc90064fc391e3dc10d260d6785
KUSHA B K <kushabaledonikrishnamurthy@1010011110011.local>
2016-08-10 16:21:32 +0530

    Initial commit

---
+++ app/src/main/java/kushabk/com/remoteui/RemoteUI.java
@@ -0,0 +1,13 @@
+package kushabk.com.remoteui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemoteUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_ui);
+    }
+}
